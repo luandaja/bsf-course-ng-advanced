6e7df5ca24ef052e253c3db9f04ac64f99d91b75
Zoe <claudia.montalvo@outlook.com>
2019-08-25 12:47:08 -0500

    adding cms and store applications

--- glotrix/apps/cms/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'cms',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/cms',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- glotrix/apps/store/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'store',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/store',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
