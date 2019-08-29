394650bb0667f4020023b0f449b76ec2c82194ee
Zoe <claudia.montalvo@outlook.com>
2019-08-28 21:29:03 -0500

    adding sizez on col component

--- newworkspace/glotrix/libs/ui/layout/src/lib/col/col.component.ts
+++ newworkspace/glotrix/libs/ui/layout/src/lib/col/col.component.ts
@@ -1,21 +1,50 @@
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation,
-  HostBinding
-} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'glotrix-col',
-  templateUrl: './col.component.html',
-  styleUrls: ['./col.component.scss']
+  template:'<ng-content></ng-content>',
+  styleUrls: ['./col.component.scss'],
+  host: { '[class]': 'className' },
 })
 export class ColComponent implements OnInit {
-  @HostBinding('class') someClass: string;
+  @Input() xs: number;
+  @Input() sm: number;
+  @Input() md: number;
+  @Input() lg: number;
+  @Input() xl: number;
 
-  constructor() {
-    this.someClass = 'col-3';
+  className : string = '';
+  sizes:  any ={
+    'xs':'col',
+    'sm':'col-sm',
+    'md':'col-md',
+    'lg':'col-lg',
+    'xl':'col-xl'
+  };
+
+  ngOnInit() {
+    this.setClassSelectors();
   }
 
-  ngOnInit() {}
+  private setClassSelectors(){
+    const selectors = this.getSelectors();
+    for (let selector of selectors) {
+      this.className += this.getClass(selector.id, selector.value);
+    }
+  }
+
+  private getSelectors() {
+    const selectors : { id: string, value: number } = [];
+    selectors.push({id:'xs',value: this.xs});
+    selectors.push({id:'sm',value: this.sm});
+    selectors.push({id:'md',value: this.md});
+    selectors.push({id:'lg',value: this.lg});
+    selectors.push({id:'xl',value: this.xl});
+    return selectors.filter(selector => selector.value !== undefined);
+  }
+
+  private getClass(id: string, value: number) : string {
+    return ` ${this.sizes[id]}-${value}`;
+  }
+
 }
