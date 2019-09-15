b51037b9537f9e59334c125bc2177797ebfcc6b5
Claudia Alejandra Montalvo Yarnold <claudia.montalvo@outlook.com>
2019-09-15 14:21:12 -0500

    fixing route to load product detail and styling product card

--- glotrix/apps/online-store/src/app/dashboard/store/products/products.selectors.ts
+++ glotrix/apps/online-store/src/app/dashboard/store/products/products.selectors.ts
@@ -1,9 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ProductsState } from './products.state';
 
-const products = createFeatureSelector<ProductsState>('products');
+const productsFeature = createFeatureSelector<ProductsState>('products');
 
 export const getProducts = createSelector(
-  products,
+  productsFeature,
   (state: ProductsState) => state.products
+);
+
+export const getProduct = (id: number) => createSelector(
+  productsFeature,
+  (state: ProductsState) => state.products.filter(product => product.id === id)[0]
 );
