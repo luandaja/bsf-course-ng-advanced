2b1342113d0c4dbd4e96358c9faa323b468d3b32
Luis Antoni <luandaja94@gmail.com>
2019-09-17 20:42:50 -0500

    Update login

--- libs/ui/login/src/lib/social-button/social-button.component.ts
+++ libs/ui/login/src/lib/social-button/social-button.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { SocialNetwork, socialNetworkStyles } from '../SocialNetworkTypes';
+import { SocialNetwork, socialNetworkStyles } from '../models/SocialNetworkTypes';
 
 @Component({
-  selector: 'ui-social-button',
-  templateUrl: './social-button.component.html',
-  styleUrls: ['./social-button.component.scss']
+	selector: 'ui-social-button',
+	templateUrl: './social-button.component.html',
+	styleUrls: ['./social-button.component.scss']
 })
 export class SocialButtonComponent {
+	@Input() text = 'Sign in';
+	@Input() socialNetwork: SocialNetwork = SocialNetwork.GooglePlus;
+	@Output() signedIn = new EventEmitter<any>();
 
-  @Input() text = 'Sign in';
-  @Input() socialNetwork: SocialNetwork = SocialNetwork.GooglePlus;
-  @Output() signedIn = new EventEmitter<any>();
+	constructor() {}
 
-  constructor() { }
+	get networkStyle() {
+		return socialNetworkStyles[this.socialNetwork];
+	}
 
-  get networkStyle() {
-    return socialNetworkStyles[this.socialNetwork];
-  }
-
-  onSignIn() {
-    this.signedIn.emit(null);
-  }
+	onSignIn() {
+		this.signedIn.emit(null);
+	}
 }
