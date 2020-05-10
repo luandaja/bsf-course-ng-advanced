3929574b60172d148eac55077ad5716abd8421e8
Claudia Alejandra Montalvo Yarnold <claudia.montalvo@outlook.com>
2020-05-09 21:19:59 -0500

    nutrition plan bootstrap

--- apps/fit-buddy/src/app/dashboard/nutrition-plan/nutrition-plan-routing.module.ts
+++ apps/fit-buddy/src/app/dashboard/nutrition-plan/nutrition-plan-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NutritionPlanComponent } from './nutrition-plan/nutrition-plan.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: NutritionPlanComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class NutritionPlanRoutingModule { }
